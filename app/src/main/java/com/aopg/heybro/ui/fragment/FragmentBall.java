598d289a7a2a00e5deee8cf97a017f943e20366f
hechuanLiu <791204944@qq.com>
2018-06-12 17:23:41 +0800

    篮球界面的完善，创建房间，加入房间的popupWindow显示

--- app/src/main/java/com/aopg/heybro/ui/fragment/FragmentBall.java
+++ app/src/main/java/com/aopg/heybro/ui/fragment/FragmentBall.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
+import android.widget.PopupWindow;
 import com.aopg.heybro.R;
+
 
 /**
  * Created by 壑过忘川 on 2018/6/7.
@@ -19,7 +21,10 @@ import com.aopg.heybro.R;
 
 public class FragmentBall extends Fragment {
     private View rootView;
-    private boolean isVisible = true;
+    private View createRoomView;
+    private View joinRoomView;
+    private PopupWindow window;
+
 
     @Nullable
     @Override
@@ -35,62 +40,66 @@ public class FragmentBall extends Fragment {
         if (parent != null) {
             parent.removeView(rootView);
         }
-         // relativeLayout = rootView.findViewById(R.id.create);
-//        relativeLayout.setVisibility(View.GONE);
-//        ballView = rootView.findViewById(R.id.ball_selected);
-//        gameView = rootView.findViewById(R.id.game_selected);
-//        gameView.setVisibility(View.GONE);
         /**
          * 创建房间
          */
-        Button btn_create = rootView.findViewById(R.id.btn_create);
+        final Button btn_create = rootView.findViewById(R.id.btn_create);
         btn_create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isVisible) {
-                    //isVisible = false;//设置其他模块不能打开
-                    Log.e("msg","123");
-                    //relativeLayout.setVisibility(View.VISIBLE);//这一句显示布局
+                createRoomView = LayoutInflater.from(getContext()).inflate(R.layout.create_room,null,false);
+                if(null == window || !window.isShowing()) {
+                    window = new PopupWindow(createRoomView, 850, 1000, true);
+                    // 设置PopupWindow是否能响应外部点击事件
+                    window.setOutsideTouchable(false);
+                    // 设置PopupWindow是否能响应点击事件
+                    window.setTouchable(true);
+                    window.showAtLocation(view, Gravity.LEFT, 20,-200);
                 }
+                /**
+                 * 关闭创建房间
+                 */
+                Button create_close = createRoomView.findViewById(R.id.create_close);
+                create_close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(null != window && window.isShowing()){
+                            window.dismiss();
+                        }
+                    }
+                });
             }
         });
-//        /**
-//         * 关闭创建房间
-//         */
-//        Button create_close = rootView.findViewById(R.id.create_close);
-//        create_close.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                relativeLayout.setVisibility(View.GONE);
-//                isVisible = true;//设置其他模块可以打开
-//            }
-//        });
-//        /**
-//         * 约球
-//         */
-//        Button btn_ball = rootView.findViewById(R.id.date_ball);
-//        btn_ball.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (isVisible) {
-//                    ballView.setVisibility(View.VISIBLE);//这一句显示布局
-//                    gameView.setVisibility(View.GONE);
-//               }
-//            }
-//        });
-//        /**
-//         * 约赛
-//         */
-//        Button btn_game = rootView.findViewById(R.id.date_game);
-//        btn_game.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (isVisible) {
-//                    gameView.setVisibility(View.VISIBLE);//这一句显示布局
-//                    ballView.setVisibility(View.GONE);
-//                }
-//            }
-//        });
+        /**
+         * 快速匹配
+         */
+        final Button btn_join = rootView.findViewById(R.id.btn_join);
+        btn_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                joinRoomView = LayoutInflater.from(getContext()).inflate(R.layout.join_room,null,false);
+                if(null == window || !window.isShowing()) {
+                    window = new PopupWindow(joinRoomView, 850, 1000, true);
+                    // 设置PopupWindow是否能响应外部点击事件
+                    window.setOutsideTouchable(false);
+                    // 设置PopupWindow是否能响应点击事件
+                    window.setTouchable(true);
+                    window.showAtLocation(view, Gravity.LEFT, 20,-200);
+                }
+                /**
+                 * 关闭快速匹配
+                 */
+                Button join_close = joinRoomView.findViewById(R.id.join_close);
+                join_close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(null != window && window.isShowing()){
+                            window.dismiss();
+                        }
+                    }
+                });
+            }
+        });
         return rootView;
     }
 }
