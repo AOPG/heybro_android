a6c1316716c6a08f8a01adbb7e168dba591a7196
王伟健 <1209050051@qq.com>
2018-06-12 09:52:32 +0800

    完成从服务器下载信息

--- app/src/main/java/com/aopg/heybro/MainActivity.java
+++ app/src/main/java/com/aopg/heybro/MainActivity.java
@@ -1,8 +1,11 @@
 package com.aopg.heybro;
 
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -32,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActivitiesManager.getInstance().addActivity(this);
+
+        startService(new Intent(this, UserInfoService.class));
 
         map = new HashMap<>();
         initTabHost();
