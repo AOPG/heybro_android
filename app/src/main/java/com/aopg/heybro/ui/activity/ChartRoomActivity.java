57111a004dda772f0076dc224dead451c7a5d9f1
王伟健 <1209050051@qq.com>
2018-06-15 09:46:24 +0800

    注销广播

---
+++ app/src/main/java/com/aopg/heybro/ui/activity/ChartRoomActivity.java
@@ -0,0 +1,16 @@
+package com.aopg.heybro.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 王伟健 on 2018-06-15.
+ */
+
+public class ChartRoomActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
