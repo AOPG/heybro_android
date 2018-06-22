f86ff7b69acd87b3984c29d2cb6fac939004ea9c
Amberyao <357782955@qq.com>
2018-06-22 10:10:11 +0800

    搜索功能完成（待添加头像）

--- app/src/main/java/com/aopg/heybro/ui/adapter/SearchfriendAdapter.java
+++ app/src/main/java/com/aopg/heybro/ui/adapter/SearchfriendAdapter.java
@@ -1,6 +1,7 @@
 package com.aopg.heybro.ui.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -19,30 +20,30 @@ public class SearchfriendAdapter extends BaseAdapter {
     private Context context;
     private int item_layout_id;
     private List<User> users;
-    public SearchfriendAdapter(Context context,int item_layout_id,List<User> users){
+    public SearchfriendAdapter(Context context, int search_friend_msg_item, List<User> users){
         this.context = context;
-        this.item_layout_id = item_layout_id;
+        this.item_layout_id = search_friend_msg_item;
         this.users = users;
     }
     @Override
     public int getCount() {
-        return 0;
+        return users.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return users.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView==null){
-            convertView = View.inflate(context, item_layout_id, null);
+            convertView = LayoutInflater.from(context).inflate(item_layout_id, null);
         }
         final TextView userName=convertView.findViewById(R.id.userName);
         final TextView userCode=convertView.findViewById(R.id.userId);
