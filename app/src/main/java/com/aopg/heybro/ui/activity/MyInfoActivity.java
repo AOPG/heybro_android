724ae8b390afd360508ad7e15ac9f47165cfc2e4
陈燕博 <117480507@qq.com>
2018-06-25 16:35:21 +0800

    我的资料数据库信息修改

--- app/src/main/java/com/aopg/heybro/ui/activity/MyInfoActivity.java
+++ app/src/main/java/com/aopg/heybro/ui/activity/MyInfoActivity.java
@@ -181,7 +181,7 @@ public class MyInfoActivity extends Activity {
                 client = HttpUtils.init(client);
                 Request request = new Request.Builder().
                        url(BUILD_URL("averageUser/updateUserInfo"+"?userCode="+ LoginInfo.user.getUserCode()+"&userNickName="+name[0]+
-                               "&userIntro="+intro[0]+"&userProvince="+ pro[0] +"&userCity="+city[0])).build();
+                               "&userIntro="+intro[0]+"&userProvince="+ pro[0] +"&userCity="+city[0]+"&birthday="+bir[0])).build();
             }
         });
 
